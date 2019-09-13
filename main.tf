f38ffdc2086d488a16da02820e88aba0d2e13557
meisterp-mms <meisterp@mediamarktsaturn.com>
2019-09-13 13:26:00 +0200

    network and project deleted

--- main.tf
+++ main.tf
@@ -25,23 +25,23 @@ resource "google_container_cluster" "gke-cluster" {
 # ---------------------------------------------------------------------------------------------------------------------
 # Project
 # ---------------------------------------------------------------------------------------------------------------------
-module "project" {
-  source          = "git@github.com:meisterp-mms/terraform-practice.git"
-  costcenter      = "${var.costcenter}"
-  project_name    = "${var.project_name}"
-  env             = "${var.env}"
-  org_group       = "${var.org_group}"
-  folder_id       = "${var.folder_id}"
-  billing_account = "${var.billing_account}"
-  services        = "${local.project_services}"
-}
+#module "project" {
+#  source          = "git@github.com:meisterp-mms/terraform-practice.git"
+#  costcenter      = "${var.costcenter}"
+#  project_name    = "${var.project_name}"
+#  env             = "${var.env}"
+#  org_group       = "${var.org_group}"
+#  folder_id       = "${var.folder_id}"
+#  billing_account = "${var.billing_account}"
+#  services        = "${local.project_services}"
+#}
 
 # ---------------------------------------------------------------------------------------------------------------------
 # Network
 # ---------------------------------------------------------------------------------------------------------------------
-module "network" {
-  source       = "git@github.com:meisterp-mms/terraform-practice.git"
-  network_name = "demonetwork"
-  region       = "${var.region}"
-  project_id   = "${module.project.project_id}"
-}
+#module "network" {
+#  source       = "git@github.com:meisterp-mms/terraform-practice.git"
+#  network_name = "demonetwork"
+#  region       = "${var.region}"
+#  project_id   = "${module.project.project_id}"
+#}
