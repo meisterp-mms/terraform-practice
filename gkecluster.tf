8b25a6beb62863d6ea8d7983a9b7f174a27190ed
meisterp-mms <meisterp@mediamarktsaturn.com>
2019-09-13 11:34:52 +0200

    renamed the cluster

--- gkecluster.tf
+++ gkecluster.tf
@@ -1,5 +1,5 @@
 resource "google_container_cluster" "gke-cluster" {
-  name               = "test_cluster"
+  name               = "test-cluster"
   network            = "default"
   zone               = "europe-west4-a"
   initial_node_count = 3
