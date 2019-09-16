cfca3f86d186e0e037d4695c5d161541e996a3af
meisterp-mms <meisterp@mediamarktsaturn.com>
2019-09-16 16:04:34 +0200

    changed kubernetes to VM

--- locals.tf
+++ locals.tf
@@ -1,6 +1,6 @@
 locals {
   project_services = [
-    "bigquery-json.googleapis.com",        // Required for cloudapis.googleapis.com, container.googleapis.com, resourceviews.googleapis.com, ...
+    "bigquery-json.googleapis.com", // Required for cloudapis.googleapis.com, container.googleapis.com, resourceviews.googleapis.com, ...
     "bigquerystorage.googleapis.com",
     "bigquerydatatransfer.googleapis.com",
     "cloudapis.googleapis.com",
@@ -14,22 +14,22 @@ locals {
     "container.googleapis.com",
     "containerregistry.googleapis.com",
     "datastore.googleapis.com",
-    "deploymentmanager.googleapis.com",    // Required for replicapool.googleapis.com, replicapoolupdater.googleapis.com
+    "deploymentmanager.googleapis.com", // Required for replicapool.googleapis.com, replicapoolupdater.googleapis.com
     "dns.googleapis.com",
     "iam.googleapis.com",
     "iamcredentials.googleapis.com",
     "logging.googleapis.com",
     "monitoring.googleapis.com",
-    "oslogin.googleapis.com",              // Required for compute.googleapis.com
+    "oslogin.googleapis.com", // Required for compute.googleapis.com
     "pubsub.googleapis.com",
     "replicapool.googleapis.com",
     "replicapoolupdater.googleapis.com",
     "resourceviews.googleapis.com",
     "runtimeconfig.googleapis.com",
     "servicecontrol.googleapis.com",
-    "servicemanagement.googleapis.com",    // Required for cloudapis.googleapis.com
+    "servicemanagement.googleapis.com", // Required for cloudapis.googleapis.com
     "serviceusage.googleapis.com",
-    "sourcerepo.googleapis.com",           // Check if needed in the future
+    "sourcerepo.googleapis.com", // Check if needed in the future
     "sqladmin.googleapis.com",
     "sql-component.googleapis.com",
     "stackdriver.googleapis.com",
--- main.tf
+++ main.tf
@@ -1,12 +1,11 @@
 # ---------------------------------------------------------------------------------------------------------------------
 # Backend
 # ---------------------------------------------------------------------------------------------------------------------
-data "google_client_config" "current" {}
 
 provider "google" {
-    credentials = "${file("account.json")}"
-    project = "TerraformTest"
-    region = "europe-west4"
+  credentials = "account.json"
+  project     = "TerraformTest"
+  region      = "europe-west4"
 }
 
 
@@ -15,9 +14,13 @@ provider "google" {
 # ---------------------------------------------------------------------------------------------------------------------
 
 resource "google_container_cluster" "test-cluster" {
-  name               = "test-cluster"
-  network            = "default"
-  location               = "europe-west4-a"
+  name                     = "test-cluster"
+  network                  = "default"
+  location                 = "europe-west4-a"
   remove_default_node_pool = true
-  initial_node_count = 3
+  initial_node_count       = 1
+  node_config {
+    machine_type = "f1-micro"
+  }
+
 }
